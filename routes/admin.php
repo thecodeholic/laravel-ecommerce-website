0e0826c51cf33651cdb5aebad851d2fb3a67040c
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-07-05 04:43:28 +0400

    Format code in admin.php routes file

--- routes/admin.php
+++ routes/admin.php
@@ -9,10 +9,10 @@
 use Illuminate\Support\Facades\Route;
 
 //Route::middleware('admin')->group(function() {
-    Route::prefix('admin')->group(function(){
-        Route::get('/', [\App\Http\Controllers\Admin\DashboardController::class, 'index']);
-        Route::get('/login', [\App\Http\Controllers\Admin\LoginController::class, 'form']);
-        Route::post('/login', [\App\Http\Controllers\Admin\LoginController::class, 'login'])
+Route::prefix('admin')->group(function () {
+    Route::get('/', [\App\Http\Controllers\Admin\DashboardController::class, 'index']);
+    Route::get('/login', [\App\Http\Controllers\Admin\LoginController::class, 'form']);
+    Route::post('/login', [\App\Http\Controllers\Admin\LoginController::class, 'login'])
         ->name('admin.login');
-    });
+});
 //});
