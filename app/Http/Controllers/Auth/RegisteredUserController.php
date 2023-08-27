90a56b9ea237aa7be30b26a80bd7f67d620cd3a0
david-salamon <david@euroart93.hr>
2023-08-27 17:23:58 +0200

    remove email ver

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,7 +45,7 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        event(new Registered($user));
+        // event(new Registered($user));
 
         Auth::login($user);
 
